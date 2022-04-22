695b9677a98e430e7608e9f255db3bd0ed0858ec
Vasika-uso <iovunicu@mail.ru>
2022-04-22 17:24:52 +0300

    added last fix

--- MAIN.cpp
+++ MAIN.cpp
@@ -174,7 +174,7 @@ glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 // load image, create texture and generate mipmaps
 int width, height, nrChannels;
 // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-unsigned char* data = stbi_load("resources/heightmaps/iceland_heightmap.png", &width, &height, &nrChannels, 4);
+unsigned char* data = stbi_load("resources/heightmaps/uppheightmap3.jpg", &width, &height, &nrChannels, 4);
 if (data)
 {
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
