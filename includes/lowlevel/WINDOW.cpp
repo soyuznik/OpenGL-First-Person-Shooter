dfcfa7e9e60cef7a00b02d03308dc4263f9887b6
Vasika-uso <iovunicu@mail.ru>
2022-04-23 20:59:15 +0300

    added main code (NEW SOURCE)

--- includes/lowlevel/WINDOW.cpp
+++
@@ -1,8 +0,0 @@
-#include "WINDOW.h"
-
-
-WINDOW::WINDOW(int width, int height, std::string name) {
-	windowSizeW = width;
-	windowSizeH = height;
-	window = glfwCreateWindow(width , height, name.c_str(), NULL, NULL);
-}
